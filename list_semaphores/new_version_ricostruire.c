f293ee147a4a063b4a44c3a68b32595ec3cf45f1
Simone Remoli <simoneremoli00@gmail.com>
2024-01-18 18:37:08 +0100

    19_09_2018
    Implementare un programma che riceva in input tramite argv[] i pathname
    associati ad N file (F1 ... FN), con N maggiore o uguale ad 1.
    Per ognuno di questi file generi un thread che gestira' il contenuto del file.
    Dopo aver creato gli N file ed i rispettivi N thread, il main thread dovra'
    leggere indefinitamente la sequenza di byte provenienti dallo standard-input.
    Ogni 5 nuovi byte letti, questi dovranno essere scritti da uno degli N thread
    nel rispettivo file. La consegna dei 5 byte da parte del main thread
    dovra' avvenire secondo uno schema round-robin, per cui i primi 5 byte
    dovranno essere consegnati al thread in carico di gestire F1, i secondi 5
    byte al thread in carico di gestire il F2 e cosi' via secondo uno schema
    circolare.
    
    L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
    WinAPI) in modo tale che quando il processo venga colpito esso dovra',
    a partire dai dati correntemente memorizzati nei file F1 ... FN, ripresentare
    sullo standard-output la medesima sequenza di byte di input originariamente
    letta dal main thread dallo standard-input.

---
+++ list_semaphores/new_version_ricostruire.c
@@ -0,0 +1,244 @@
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int contatore_acquisizioni;
+int contatore_appoggio = 0;
+int indexer = 0;
+int conta_val = 0;
+
+int numero_threads;
+char** file_names;
+int check(int, char**);
+FILE** source_files;
+int descriptor;
+char buffer[5][1024];
+int semaforo_main;
+int contatore_nuovo = 0;
+int array_semaforico;
+void* worker(void*);
+int counter = 0;
+struct nodo
+{
+	char buffered[1024];
+	struct nodo* p_next;
+};
+
+struct nodo* testa = NULL;
+struct nodo* app = NULL;
+void handle(int);
+
+int main(int argc, char **argv)
+{
+	struct sembuf oper;
+	int ret;
+	pthread_t pid;
+	if(check(argc,argv))
+	{
+		fprintf(stderr, "Errore [*] - Devi necessariamente passare un argomento [*] \n");
+		printf("Aborted [*] \n");
+		return 0;
+	}
+	file_names = argv;
+	numero_threads = argc - 1;
+	printf("Numero di argomenti passati = %d \n", numero_threads);
+	source_files = (FILE**)malloc(sizeof(FILE*)*numero_threads);
+	semaforo_main = semget(IPC_PRIVATE, 1, IPC_CREAT|IPC_EXCL|0666);
+	array_semaforico = semget(IPC_PRIVATE, numero_threads, IPC_CREAT|IPC_EXCL|0666);
+	semctl(semaforo_main, 0, SETVAL, 1); //[1]
+	for(int i=0;i<numero_threads;i++)
+	{
+		semctl(array_semaforico, i, SETVAL, 0); //[0][0][0][0]
+	}
+
+	for(int i=0;i<numero_threads;i++)
+	{
+		printf("File [%d] = %s \n", i+1, file_names[i+1]);
+    	descriptor = open(file_names[i+1], O_CREAT|O_RDWR|O_TRUNC, 0666);
+		source_files[i] = fdopen(descriptor, "w+");
+
+	}
+
+	for(long i=0;i<numero_threads;i++)
+	{
+		pthread_create(&pid, NULL, worker, (void*)i);
+	}
+
+
+
+	signal(SIGINT, handle);
+
+	while(1)
+	{
+		oper.sem_num = 0;
+		oper.sem_op = -1;
+		oper.sem_flg = 0;
+redo1:
+		ret = semop(semaforo_main, &oper, 1);
+		if(ret == -1)
+			if(errno == EINTR)
+				goto redo1;
+
+
+		for(int i=0;i<5;i++)
+		{
+			printf("Inserisci nella [%d] acquisizione [*] \n", i+1);
+			printf(">>");
+
+			if(fgets(buffer[i], 1024, stdin))
+				buffer[i][strcspn(buffer[i], "\n")] = 0;
+
+
+			
+		}
+		contatore_acquisizioni = contatore_acquisizioni + 1;
+		printf("Numero acquisizioni = %d \n ", contatore_acquisizioni);
+
+
+		oper.sem_num = counter;
+		oper.sem_op = 1;
+		oper.sem_flg = 0;
+redo2:
+		ret = semop(array_semaforico, &oper, 1);
+		if(ret == -1)
+			if(errno == EINTR)
+				goto redo2;
+
+		counter = counter + 1;
+		if(counter == numero_threads)
+			counter = 0;
+	}
+	return 0;
+}
+int check(int argc, char **argv)
+{
+	if(argc==1)
+		return 1;
+	if(argc>1)
+		return 0;
+}
+
+void* worker(void* arg)
+{
+	struct sembuf oper;
+	int ret;
+	long pid = (long) arg;
+
+
+	while(1)
+	{
+
+		oper.sem_num = pid;
+		oper.sem_op = -1;
+		oper.sem_flg = 0;
+redo1:
+		ret = semop(array_semaforico, &oper, 1);
+		if(ret == -1)
+			if(errno == EINTR)
+				goto redo1;
+
+		printf("\n");
+		printf("Sono il thread numero [%ld]\n", pid+1);
+		for(int i=0;i<5;i++)
+		{
+			//printf(" Valore inserito = %s \n", buffer[i]);
+			fprintf(source_files[pid], "%s\n", buffer[i]);
+			fflush(source_files[pid]);
+			struct nodo* nuovo_nodo = malloc(sizeof(struct nodo));
+			strncpy(nuovo_nodo->buffered, buffer[i], 1024 );
+
+			if(testa == NULL)
+			{
+				app = nuovo_nodo;
+				testa = app;
+			}
+			else
+			{
+				app->p_next = nuovo_nodo;
+				app = nuovo_nodo;
+			}
+		}
+		printf("Updated %s [*]\n", file_names[pid+1]);
+		printf("\n");
+
+
+
+		oper.sem_num = 0;
+		oper.sem_op = 1;
+		oper.sem_flg = 0;
+redo2:
+		ret = semop(semaforo_main, &oper, 1);
+		if(ret == -1)
+			if(errno == EINTR)
+				goto redo2;
+
+	}
+}
+
+void handle(int unused)
+{
+	printf("\n");
+	struct nodo* temp = testa;
+	char pointer[1024];
+	while(temp!=NULL)
+	{
+		printf("%s\n", temp->buffered);
+		temp = temp->p_next;
+	}
+	printf("\n");
+
+	printf("  -  Visualizzazione alternativa - [*] \n");
+
+	for(int i=0;i<numero_threads;i++)
+	{
+		fseek(source_files[i],0,0);
+	}
+
+
+	indexer = 0;
+	contatore_appoggio = 0;
+	contatore_nuovo = 0;
+
+
+
+	//printf("----- APERTURA FILE [%d] ----- \n", indexer);
+
+
+	while( EOF != fscanf(source_files[indexer], "%[^\n]\n", pointer))
+	{
+		//printf("Vaore di indexer = %d \n", indexer);
+		printf(" \t  %s \n ", pointer);
+		//printf("i =  %d \n", indexer);
+		if(contatore_appoggio == 4)
+		{
+			indexer = indexer + 1;
+			contatore_appoggio = 0;
+			//printf("indexer = %d \n", indexer);
+			//printf("Contatore acquisizioni = %d \n", contatore_acquisizioni); // se supera 4 index va in core dumped, perchè i file sono 4 
+
+			if(indexer == numero_threads)
+			{
+				indexer = 0;
+				contatore_appoggio = 0;
+			}
+
+		}
+		else
+		{
+			contatore_appoggio = contatore_appoggio + 1;
+		}
+		
+	}
+
+	
+}
